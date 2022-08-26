3ab4e3e5019a91554fb4bdd619b570d257092a4d
Nikolay Balov <nhbalov@gmail.com>
2022-08-25 21:27:36 -0500

    Initial commit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get("/about", function(req, res) {
 	res.render("about");
 });
 
+
 //app.listen(process.env.PORT, process.env.IP);
 // go to http://localhost:8080
 app.listen(process.env.PORT || 8080, function() {
